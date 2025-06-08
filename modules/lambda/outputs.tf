0fe3486790c903727a2a43a962c3574ac43cbe5b
adiConsultadd <aditya.rathore@consultadd.com>
2025-06-08 21:37:43 +0530

    Added Lambdas 2 and 3

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -7,3 +7,23 @@ output "lambda_1_name" {
   description = "Lambda 1 name"
   value       = aws_lambda_function.lambda_1.function_name
 }
+
+output "lambda_2_arn" {
+  description = "Lambda 2 ARN"
+  value       = aws_lambda_function.lambda_2.arn
+}
+
+output "lambda_2_name" {
+  description = "Lambda 2 name"
+  value       = aws_lambda_function.lambda_2.function_name
+}
+
+output "lambda_3_arn" {
+  description = "Lambda 3 ARN"
+  value       = aws_lambda_function.lambda_3.arn
+}
+
+output "lambda_3_name" {
+  description = "Lambda 3 name"
+  value       = aws_lambda_function.lambda_3.function_name
+}
