3fdb4af261e4c2e6c11516427b52502539ef087a
adiConsultadd <aditya.rathore@consultadd.com>
2025-06-12 18:16:38 +0530

    Added: Backend configuration

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "blackbox-terraform-state-bucket"          
-    region = "us-east-1"
-    encrypt = true
-    # dynamodb_table = "my-terraform-locks"     
+    bucket         = "blackbox-terraform-state-bucket"
+    workspace_key_prefix = "blackbox"
+    key            = "terraform.tfstate" 
+    region         = "us-east-1"
+    encrypt        = true
+    # dynamodb_table = "blackbox-terraform-locks"        
   }
 }
