4915fafa8224ff2d6bb9ae275e2cfa2b83193efb
adiConsultadd <aditya.rathore@consultadd.com>
2025-06-15 18:42:59 +0530

    FIXED: Outputs of lambda-sourcing

--- outputs.tf
+++ outputs.tf
@@ -3,33 +3,12 @@ output "sourcing_s3_bucket" {
   value       = module.sourcing.s3_bucket_name
 }
 
-output "lambda_names" {
-  description = "Map of Lambda function names"
-  value = {
-    for k, inst in module.sourcing.lambda :
-    k => inst.lambda_name
-  }
-}
-
-output "lambda_arns" {
-  description = "Map of Lambda function ARNs"
-  value = {
-    for k, inst in module.sourcing.lambda :
-    k => inst.lambda_arn
-  }
-}
-
-output "cdn_distribution_id" {
-  description = "CloudFront distribution ID"
-  value       = module.sourcing.cloudfront.distribution_id
-}
-
 output "cdn_domain_name" {
   description = "CloudFront distribution domain name"
-  value       = module.sourcing.cloudfront.distribution_domain_name
+  value       = module.sourcing.cloudfront_domain
 }
 
 output "eventbridge_rule_arn" {
   description = "ARN of the EventBridge scheduled rule"
-  value       = module.sourcing.eventbridge.eventbridge_rule_arn
+  value       = module.sourcing.eventbridge_rule_arn
 }
