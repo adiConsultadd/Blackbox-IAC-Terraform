ac301b5250862bba740512dbf2ae5371da7f1f05
adiConsultadd <aditya.rathore@consultadd.com>
2025-06-15 14:11:37 +0530

    ADDED : New modules to sourcing

--- modules/features/sourcing/main.tf
+++ modules/features/sourcing/main.tf
@@ -10,59 +10,59 @@ module "s3" {
 ###############################################################################
 # 2. Lambdas (three functions)
 ###############################################################################
-# locals {
-#   lambdas = {
-#     lambda_1 = {
-#       source_dir = "${path.module}/lambda-code/lambda-1"
-#       env        = { EXAMPLE_ENV_VAR = "HelloWorld-lambda1" }
-#     }
-#     lambda_2 = {
-#       source_dir = "${path.module}/lambda-code/lambda-2"
-#       env        = { EXAMPLE_ENV_VAR = "HelloWorld-lambda2" }
-#     }
-#     lambda_3 = {
-#       source_dir = "${path.module}/lambda-code/lambda-3"
-#       env        = { EXAMPLE_ENV_VAR = "HelloWorld-lambda3" }
-#     }
-#   }
-# }
+locals {
+  lambdas = {
+    lambda_1 = {
+      source_dir = "${path.module}/lambda-code/lambda-1"
+      env        = { EXAMPLE_ENV_VAR = "HelloWorld-lambda1" }
+    }
+    lambda_2 = {
+      source_dir = "${path.module}/lambda-code/lambda-2"
+      env        = { EXAMPLE_ENV_VAR = "HelloWorld-lambda2" }
+    }
+    lambda_3 = {
+      source_dir = "${path.module}/lambda-code/lambda-3"
+      env        = { EXAMPLE_ENV_VAR = "HelloWorld-lambda3" }
+    }
+  }
+}
 
-# module "lambda" {
-#   source = "../../base-infra/lambda"
-#   for_each = local.lambdas
+module "lambda" {
+  source = "../../base-infra/lambda"
+  for_each = local.lambdas
 
-#   function_name         = "${var.project_name}-${var.environment}-${each.key}"
-#   source_dir            = each.value.source_dir
-#   lambda_role_arn       = var.lambda_role_arn
-#   environment_variables = each.value.env
-# }
+  function_name         = "${var.project_name}-${var.environment}-${each.key}"
+  source_dir            = each.value.source_dir
+  lambda_role_arn       = var.lambda_role_arn
+  environment_variables = each.value.env
+}
 
 ###############################################################################
 # 3. EventBridge – triggers lambda‑1 every day (example)
 ###############################################################################
-# module "eventbridge" {
-#   source       = "../../base-infra/eventbridge"
-#   environment  = var.environment
-#   project_name = var.project_name
+module "eventbridge" {
+  source       = "../../base-infra/eventbridge"
+  environment  = var.environment
+  project_name = var.project_name
 
-#   lambda_arn_to_trigger = module.lambda["lambda_1"].lambda_arn
-#   schedule_expression   = var.eventbridge_schedule_expression
-# }
+  lambda_arn_to_trigger = module.lambda["lambda_1"].lambda_arn
+  schedule_expression   = var.eventbridge_schedule_expression
+}
 
 ###############################################################################
 # 4. CloudFront 
 ###############################################################################
-# module "cloudfront" {
-#   source       = "../../base-infra/cloudfront"
-#   environment  = var.environment
-#   project_name = var.project_name
+module "cloudfront" {
+  source       = "../../base-infra/cloudfront"
+  environment  = var.environment
+  project_name = var.project_name
 
-#   s3_bucket_name         = module.s3.bucket_name
-#   price_class            = var.cloudfront_price_class
-#   viewer_protocol_policy = var.viewer_protocol_policy
-#   default_root_object    = var.default_root_object
-#   enabled                = var.cloudfront_enabled
-# }
+  s3_bucket_name         = module.s3.bucket_name
+  price_class            = var.cloudfront_price_class
+  viewer_protocol_policy = var.viewer_protocol_policy
+  default_root_object    = var.default_root_object
+  enabled                = var.cloudfront_enabled
+}
 
 ###############################################################################
 # 5. RDS 
