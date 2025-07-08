eb2f305c143fc53be146fc250e1c1b272cad756b
adiConsultadd <aditya.rathore@consultadd.com>
2025-07-08 17:05:09 +0530

    DELETED : Unused IAM And SSM Module

--- modules/base-infra/ssm/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "The full name of the SSM parameter"
-  value       = aws_ssm_parameter.this.name
-}
-
-output "arn" {
-  description = "The ARN of the SSM parameter"
-  value       = aws_ssm_parameter.this.arn
-}
