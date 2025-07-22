4d0f00f185165d89216c10ddf34eaa6a7c48b5f4
adiConsultadd <aditya.rathore@consultadd.com>
2025-07-22 22:45:51 +0530

    Changed : The Lambda Environment For One Of The Lambda

--- modules/services/costing/main.tf
+++ modules/services/costing/main.tf
@@ -26,7 +26,10 @@ module "costing_lambda_role" {
 # 2. Lambda functions (referencing the single service role)
 ###############################################################################
 module "lambda" {
-  for_each = { for k, v in var.lambdas : k => v if k != "costing-hourly-wages" }
+  for_each = {
+    for k, v in var.lambdas : k => v
+    if k != "costing-cost-step-machine" && k != "costing-hourly-wages"
+  }
 
   source        = "../../base-infra/lambda"
   function_name = "${var.project_name}-${var.environment}-${each.key}"
@@ -130,4 +133,28 @@ module "costing_state_machine_2" {
     rfp_cost_image_extractor_lambda_arn   = module.lambda["costing-rfp-cost-image-extractor"].lambda_arn
     rfp_cost_image_calculation_lambda_arn = module.lambda["costing-rfp-cost-image-calculation"].lambda_arn
   })
+}
+
+module "costing_cost_step_machine_lambda" {
+  source        = "../../base-infra/lambda"
+  function_name = "${var.project_name}-${var.environment}-costing-cost-step-machine"
+  
+  # Configuration is pulled directly from the variable for this specific lambda
+  runtime     = var.lambdas["costing-cost-step-machine"].runtime
+  timeout     = var.lambdas["costing-cost-step-machine"].timeout
+  memory_size = var.lambdas["costing-cost-step-machine"].memory_size
+  layers      = [for layer_key in var.lambdas["costing-cost-step-machine"].layers : var.available_layer_arns[layer_key]]
+  
+  environment_variables = {
+    STEP_FUNCTION_ARN = module.costing_state_machine_1.state_machine_arn
+    SSM_PREFIX        = "blackbox-${var.environment}"
+  }
+
+  lambda_role_arn  = module.costing_lambda_role.role_arn
+  s3_bucket        = var.placeholder_s3_bucket
+  s3_key           = var.placeholder_s3_key
+  source_code_hash = var.placeholder_source_code_hash
+
+  vpc_subnet_ids         = var.private_subnet_ids
+  vpc_security_group_ids = [var.lambda_security_group_id]
 }
